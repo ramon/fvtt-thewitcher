eb813bd05df29d0a142ec7267b54124a33a4a9c6
Ramon Soares <ramongsoares@gmail.com>
2021-01-13 16:54:04 -0300

    add initial item structure

--- src/module/item/item-sheet.ts
+++ src/module/item/item-sheet.ts
@@ -1,1 +1,51 @@
-export default class WitcherItemSheet extends ItemSheet {}
+export default class WitcherItemSheet extends ItemSheet {
+    /** @override */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 560, height: 400,
+            classes: ["thewitcher", "sheet", "item"],
+            resizable: true,
+            scrollY: [".tab.details"],
+            tabs: [{navSelector: ".tabs", contentSelector: ".sheet-body", initial: "description"}]
+        })
+    }
+
+    /** @override */
+    get template() {
+        return `systems/thewitcher/templates/item/${this.item.data.type}.hbs`;
+    }
+
+    /** @override */
+    // @ts-ignore-start Property_0_does_not_exist_on_type_1
+    getData(options) {
+        // @ts-ignore
+        const data = super.getData(options);
+
+        // @ts-ignore
+        data.config = CONFIG.WITCHER;
+
+        // Item Type, Status and Details
+        // @ts-ignore
+        data.itemType = game.i18n.localize(`THEWITCHER.item.type.${data.item.type}`);
+        // @ts-ignore
+        data.itemStatus = WitcherItemSheet.getItemStatus(data.item);
+        // @ts-ignore
+        data.itemProperties = WitcherItemSheet.getItemProperties(data.item)
+        // @ts-ignore
+        data.isPhysical = data.item.data.hasOwnProperty("quantity")
+        // @ts-ignore
+        data.isConcealable = data.item.data.hasOwnProperty("concealment")
+
+        return data;
+    }
+
+    private static getItemStatus(item): void {
+        if (["service", "general"].includes(item.type)) return;
+    }
+
+    private static getItemProperties(item) {
+        const props = [];
+
+        return props.filter(p => !!p);
+    }
+}
--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -2,11 +2,16 @@ export const preloadTemplates = async function() {
 	const templatePaths = [
 		// Add paths to "systems/thewitcher/templates"
 
+		// Actor
 		"systems/thewitcher/templates/actor/parts/sheet-header.hbs",
 		"systems/thewitcher/templates/actor/parts/sheet-npc-header.hbs",
 		"systems/thewitcher/templates/actor/parts/actor-skills.hbs",
 		"systems/thewitcher/templates/actor/parts/actor-skill-list-table.hbs",
-		"systems/thewitcher/templates/actor/parts/actor-professions.hbs"
+		"systems/thewitcher/templates/actor/parts/actor-professions.hbs",
+
+		// Items
+		"systems/thewitcher/templates/item/parts/description.hbs",
+		"systems/thewitcher/templates/item/parts/summary.hbs",
 	];
 
 	return loadTemplates(templatePaths);
